e1635245c226b5c91c39b407599727adc3e76901
Just-An-Average-Toaster <114497348+Just-An-Average-Toaster@users.noreply.github.com>
2023-03-14 10:08:23 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -2,18 +2,34 @@ namespace SpriteKind {
     export const hospitaldoor = SpriteKind.create()
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.hospitaldoor, function (sprite, otherSprite) {
-    tileUtil.unloadTilemap()
-    tiles.setCurrentTilemap(tilemap`Hosptial_Interior`)
-    Tomothy_Map.x = 23
-    Tomothy_Map.y = 19
+    if (inhospital == 0) {
+        tileUtil.unloadTilemap()
+        tiles.setCurrentTilemap(tilemap`Hosptial_Interior`)
+        Tomothy_Map.y = 374
+        Tomothy_Map.x = 320
+        inhospital = 1
+        makedoorswork()
+    } else if (inhospital == 1) {
+        tileUtil.unloadTilemap()
+        tiles.setCurrentTilemap(tilemap`cityEnemy0`)
+        Tomothy_Map.y = 600
+        Tomothy_Map.x = 256
+        inhospital = 0
+        makedoorswork()
+    }
 })
+function makedoorswork () {
+    sprites.destroyAllSpritesOfKind(SpriteKind.hospitaldoor)
+    tileUtil.createSpritesOnTiles(assets.tile`Door0`, assets.image`doorimg`, SpriteKind.hospitaldoor)
+    tileUtil.createSpritesOnTiles(assets.tile`Door1`, assets.image`doorimg0`, SpriteKind.hospitaldoor)
+}
+let inhospital = 0
 let Tomothy_Map: Sprite = null
-tiles.setCurrentTilemap(tilemap`cityEnemy`)
+tiles.setCurrentTilemap(tilemap`cityEnemy0`)
 Tomothy_Map = sprites.create(assets.image`Tomothy`, SpriteKind.Player)
 controller.moveSprite(Tomothy_Map)
 scene.cameraFollowSprite(Tomothy_Map)
 let Hospitalinterior = tilemap`Hosptial_Interior`
 let city = tilemap`cityEnemy0`
-tileUtil.createSpritesOnTiles(assets.tile`Door0`, assets.image`doorimg`, SpriteKind.hospitaldoor)
-tileUtil.createSpritesOnTiles(assets.tile`Door1`, assets.image`doorimg0`, SpriteKind.hospitaldoor)
+makedoorswork()
 tileUtil.connectMaps(city, Hospitalinterior, MapConnectionKind.Door1)
